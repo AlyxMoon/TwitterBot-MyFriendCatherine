3a147f59c7aae36112b3675c32e3e86aa542a705
AlyxMoon <allisterkmoon@gmail.com>
2016-06-09 23:01:55 -0700

    Now pulls tweets
    Progress! It now successfully pulls the very first tweet from a search
    result.
    The search query is an exact match of 'my cat'

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
 import tweepy
+from secrets import *
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+result = api.search(q = '"my cat"', count = 1, rpp = 1)
+print result
