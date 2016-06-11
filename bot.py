332bcf5e7212c23aa7ec425c68a874c9dc825ff8
AlyxMoon <allisterkmoon@gmail.com>
2016-06-10 19:47:23 -0700

    Schedule the Bot
    Now it runs on a schedule, probably.

--- bot.py
+++ bot.py
@@ -2,7 +2,11 @@ import tweepy
 import re
 import os
 
+# Create the scheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+sched = BlockingScheduler()
 
+# Good ol' environment variables with our secret info
 API_KEY = os.environ['API_KEY']
 API_SECRET = os.environ['API_SECRET']
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
@@ -13,25 +17,30 @@ auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-# Search!
-result = api.search(q = '"my cat"', count = 1, rpp = 1, lang = 'en')
-text = result[0].text
-#text = 'my cat is a total square, my cat'
 
-# Filter out undesirable parts of string (such as mentions and urls)
+def doTweet():
+	# Search!
+	result = api.search(q = '"my cat"', count = 1, rpp = 1, lang = 'en')
+	text = result[0].text
+	#text = 'my cat is a total square, my cat'
 
-# Get rid of URL's completely
-text = re.sub('http\S+', '', text)
-# Remove mentions
-text = re.sub('@\S+', '', text)
-# Remove # and any text associated with them
-# Extra join and split is to remove resulting extra whitespace
-text = ' '.join(re.sub('#\S+', '', text).split())
+	# Filter out undesirable parts of string (such as mentions and urls)
 
-# Catherinize it
-pattern = re.compile('(?:^|\W)my cat(?:$|\W)', re.IGNORECASE)
-text = ' '.join(re.sub(pattern, ' my friend Catherine ', text).split())
+	# Get rid of URL's completely
+	text = re.sub('http\S+', '', text)
+	# Remove mentions
+	text = re.sub('@\S+', '', text)
+	# Remove # and any text associated with them
+	# Extra join and split is to remove resulting extra whitespace
+	text = ' '.join(re.sub('#\S+', '', text).split())
 
-# It's done!
-print u'%s' % text
-api.update_status(text)
+	# Catherinize it
+	pattern = re.compile('(?:^|\W)my cat(?:$|\W)', re.IGNORECASE)
+	text = ' '.join(re.sub(pattern, ' my friend Catherine ', text).split())
+
+	# It's done!
+	print u'%s' % text
+	api.update_status(text)
+
+# Last but not least, schedule the task to run every 30 minutes
+sched.addJob(doTweet, "Catherine-ize a Tweet", minutes = 30)
